11e7ef75ef3fee73db0d8ea786db3bacd1029d15
Cauê Magela <111535805+cauemmo@users.noreply.github.com>
2025-04-14 15:00:14 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,19 +1,16 @@
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
-# Carrega as variáveis do .env
 load_dotenv()
 
-# Lê os dados do banco do .env
 host = os.getenv("POSTGRES_HOST")
 port = os.getenv("POSTGRES_PORT")
 db = os.getenv("POSTGRES_DB")
 user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
+password = quote_plus(os.getenv("POSTGRES_PASSWORD"))  # codifica os caracteres especiais!
 
-# Cria a string de conexão
 DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
 
-# Cria o engine para se conectar ao banco
 engine = create_engine(DATABASE_URL)
